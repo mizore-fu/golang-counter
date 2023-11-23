b7c7021ee29e063863a2d72ad39684011753767f
mizore-fu <mhxxname0782@gmail.com>
2023-11-23 17:00:54 +0900

    カウントを増減するエンドポイントを追加

--- main.go
+++ main.go
@@ -18,9 +18,29 @@ func main() {
 
 	e := echo.New()
 	e.GET("/count", GetCount)
+	e.PUT("/countup", IncreaseCount)
+	e.PUT("/countdown", DecreaseCount)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func GetCount(c echo.Context) error {
 	return c.JSON(http.StatusOK, count)
+}
+
+func IncreaseCount(c echo.Context) error {
+	newCount := &Count{
+		Count: count.Count + 1,
+	}
+	count = newCount
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func DecreaseCount(c echo.Context) error {
+	newCount := &Count{
+		Count: count.Count - 1,
+	}
+	count = newCount
+
+	return c.NoContent(http.StatusNoContent)
 }
